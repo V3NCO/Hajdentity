04cfb0a1e203d2d5a0b65d0153791358d33c90d1
Venco <v3nco@v3nco.dev>
2026-05-03 01:24:23 +0200

    hihi

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true
+    }
+  },
   routeRules: {
       '/api/**': {
         proxy: "http://127.0.0.1:8000/api/**"
