648f63b998812a98b0979a634264e0ea0c0e36ab
Venco <v3nco@v3nco.dev>
2026-05-02 22:01:38 +0200

    auth code somewhat but im not sure it workds im trying to figure out an accessible way to write to the nfc tag

--- api/app.py
+++ api/app.py
@@ -1,11 +1,15 @@
 from contextlib import asynccontextmanager
-from typing import Any
+from logging import raiseExceptions
 
-from fastapi import FastAPI
-from fastapi.routing import APIRouter
+from fastapi import FastAPI, HTTPException, APIRouter
 from piccolo.engine import engine_finder
-from piccolo_api.crud.serializers import create_pydantic_model
-from home.tables import Task
+
+from pydantic import BaseModel
+from Crypto.Cipher import AES
+from Crypto.Hash import CMAC
+import binascii
+from home.tables import NFCTable
+
 
 
 async def open_database_connection_pool():
@@ -34,20 +38,44 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 api = APIRouter(prefix="/api")
 
-TaskModelIn: Any = create_pydantic_model(
-    table=Task,
-    model_name="TaskModelIn",
-)
 
-TaskModelOut: Any = create_pydantic_model(
-    table=Task,
-    include_default_columns=True,
-    model_name="TaskModelOut",
-)
+
+class NfcRequest(BaseModel):
+    uid: str
+    ctr: int
+    mac: str
 
 @api.get("/")
 async def test():
     return "pong!"
+
+@api.post("/nfc/auth")
+async def nfc_auth(tap: NfcRequest):
+    tag = await NFCTable.objects().get(NFCTable.uid == tap.uid)
+    if not tag:
+        raise HTTPException(status_code=404, detail="Tag not found")
+
+    if tap.ctr <= tag.last_counter:
+        raise HTTPException(status_code=403, detail="URL Already used - Please physically tap the NFC Tag!")
+
+    try:
+        key = tag.secret_key
+        message = binascii.unhexlify(tap.uid) + tap.ctr.to_bytes(3, 'little')
+
+        cobj = CMAC.new(key, ciphermod=AES)
+        cobj.update(message)
+        expected_mac = cobj.hexdigest()[:16]
+
+        if tap.mac.lower() != expected_mac.lower():
+            raise HTTPException(status_code=403, detail="Invalid MAC signature")
+
+        tag.last_counter = tap.ctr
+        await tag.save()
+
+        return { "status": "success", "message": "One-time tap verified" }
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 app.include_router(api)
---
+++ frontend/app/pages/nfc/auth.vue
@@ -0,0 +1,23 @@
+<script setup>
+const route = useRoute()
+const { uid, ctr, mac } = route.query
+
+const { data, error } = await useFetch('/api/nfc/auth', {
+  method: 'POST',
+  body: {
+    uid: uid,
+    ctr: parseInt(ctr, 16),
+    mac: mac
+  }
+})
+
+if (data.value) {
+  console.log("Auth success")
+} else {
+  console.log("nope")
+}
+</script>
+
+<template>
+
+</template>
