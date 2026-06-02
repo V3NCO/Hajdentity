d9caf4e83080cb5c299178c3143fe98d12cfe9a5
Venco <v3nco@v3nco.dev>
2026-06-03 00:48:24 +0200

    Start doing ui stuff and fix pattern :3

--- frontend/app/pages/register/index.vue
+++ frontend/app/pages/register/index.vue
@@ -12,23 +12,91 @@ useSeoMeta({
   twitterCard: 'summary'
 })
 
-useHead({
-  htmlAttrs: {
-    lang: 'en'
-  },
-  link: [
-    {
-      rel: 'icon',
-      type: 'image/png',
-      href: '/favicon.png'
-    }
-  ]
-})
+useHead({ htmlAttrs: { lang: 'en' } })
+
+const registerForm = ref<HTMLFormElement | null>(null)
+const username = ref('')
+const email = ref('')
+const password = ref('')
+
+async function onSubmit() {
+  const form = registerForm.value
+  if (!form) return
+
+  if (!form.checkValidity()) {
+    form.reportValidity()
+    return
+  }
+
+  await $fetch("/api/auth/register", { method: 'POST', body: { username: username.value, email: email.value, password: password.value } })
+}
+
 </script>
 
 <template>
-  <input id="username">
-  <input id="email">
-  <input id="password" type="password">
-  <input id="confirm-password" type="password">
+<div class="rcont">
+  <div class="vcont">
+    <form ref="registerForm" @submit.prevent="onSubmit">
+      <input
+        v-model="username"
+        id="username"
+        name="username"
+        type="text"
+        minlength=3
+        maxlength=96
+        pattern="(?:[A-Za-z0-9_]|-){3,96}"
+        required
+      />
+      <input v-model="email" id="email" name="email" type="email" required/>
+      <input v-model="password" id="password" name="password" type="password" minlength=16 required/>
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</div>
 </template>
+
+<style>
+html, body {
+  height: 100%;
+  margin: 0;
+}
+
+body {
+  overflow: hidden;
+  background-image: url("/stocksharks.jpeg");
+  /*Eventually this would be an animated carousel of shark images from users*/
+}
+
+#app, #__nuxt {
+  height: 100%;
+}
+
+.rcont, .vcont {
+  box-sizing: border-box;
+}
+
+.rcont {
+  width: 50vw;
+  height: 100svh;
+  position: relative;
+  float: right;
+  padding: 1em;
+  overflow: hidden;
+}
+
+.vcont {
+    padding: 1em;
+    height: 100%;
+    width: 100%;
+    background-image: linear-gradient(147deg, #F1CBBFCC, #F3A9BACC);
+    backdrop-filter: blur(10px);
+    -webkit-backdrop-filter: blur(10px);
+    display: flex;
+    border-radius: 18px;
+    border: 1px solid rgba(255, 255, 255, 0.3);
+}
+
+input:user-invalid {
+  box-shadow: 0 0 5px 1px red;
+}
+</style>
