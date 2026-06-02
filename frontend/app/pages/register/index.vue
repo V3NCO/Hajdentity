007fdc6a2d9ad0e643eb545b042d7e51d02e22df
Venco <v3nco@v3nco.dev>
2026-06-02 17:30:02 +0200

    Create index.vue

---
+++ frontend/app/pages/register/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Register',
+  description: 'Make a new account on Hajdentity!',
+  ogTitle: 'Register',
+  ogDescription: 'Make a new account on Hajdentity!',
+  ogImage: '[og:image]',
+  ogUrl: 'https://id.blahaj.engineering/register',
+  twitterTitle: 'Register',
+  twitterDescription: 'Make a new account on Hajdentity!',
+  twitterImage: '[twitter:image]',
+  twitterCard: 'summary'
+})
+
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png'
+    }
+  ]
+})
+</script>
+
+<template>
+  <input id="username">
+  <input id="email">
+  <input id="password" type="password">
+  <input id="confirm-password" type="password">
+</template>
