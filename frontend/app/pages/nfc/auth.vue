70c16753c1606b9f3273b0039e8f910a8bf07b19
Venco <v3nco@v3nco.dev>
2026-07-11 01:21:50 +0200

    NFC frontend side works!

--- frontend/app/pages/nfc/auth.vue
+++ frontend/app/pages/nfc/auth.vue
@@ -1,22 +1,32 @@
-<script setup>
+<script setup lang="ts">
 const route = useRoute()
 const { picc_data, cmac } = route.query
 
-const { data, error } = await useFetch('/api/nfc/auth', {
-  method: 'POST',
-  body: {
-    picc_data: picc_data,
-    cmac: cmac
+import loadingCss from '~/assets/css/loading.css?raw'
+useHead({ htmlAttrs: { lang: 'en' }, style: [{ textContent: loadingCss }] })
+
+
+const haj = ref()
+onMounted(async () => {
+  try {
+    const res = await $fetch<{ status: string; haj: string }>('/api/nfc/auth', {
+      method: 'POST',
+      body: {picc_data, cmac},
+      credentials: 'include'
+    })
+    haj.value = res.haj
+    await navigateTo(`/plush/${haj.value}/act`)
+  } catch (e) {
+    alert((e as any)?.response?._data?.detail || (e as any)?.data?.detail || 'An error occured')
   }
 })
-
-if (data.value) {
-  console.log("Auth success?!111!!!!! YIPPPEEEEE")
-} else {
-  console.log("nope")
-}
 </script>
 
 <template>
-
+<Transition><div class="loading">
+  <h1>Loading...</h1>
+  <video autoplay loop muted playsinline>
+    <source src="/blahaj.webm" type="video/webm">
+  </video>
+</div></Transition>
 </template>
